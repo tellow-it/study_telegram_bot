84f60e8ac834aea8eba12480d2b57fb8cc40a1f1
Тихонов Иван А <iatikhonov@f-soft.org~>
2023-02-12 15:08:23 +0300

    filtres

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,3 +1,5 @@
+import json
+
 from aiogram import Bot
 from aiogram.types import Message
 
@@ -6,3 +8,15 @@ async def get_start(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, f'Привет {message.from_user.first_name}. Добро пожаловать!')
     await message.answer(f'Привет {message.from_user.first_name}. Добро пожаловать!')
     await message.reply(f'Привет {message.from_user.first_name}. Добро пожаловать!')
+
+
+async def get_photo(message: Message, bot: Bot):
+    await message.answer('Отлично, ты отправил фото, сохраня ее...')
+    file = await bot.get_file(message.photo[-1].file_id)
+    await bot.download_file(file.file_path, f'{message.photo[-1].file_id}.jpg')
+
+
+async def get_hello(message: Message, bot: Bot):
+    await message.answer(f'И тебе привет!')
+    json_str = json.dumps(message.dict(), default=str)
+    print(json_str)
